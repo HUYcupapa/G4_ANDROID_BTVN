32ef7488cc8d03d8622ae564ef35129f41aa46df
Vương Việt Cường <112933836+vietcuong2004@users.noreply.github.com>
2025-02-23 22:33:22 +0700

    Update LopHoc.java bo bot thong tin

--- BT1_android/src/bt1_android/LopHoc.java
+++ BT1_android/src/bt1_android/LopHoc.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bt1_android;
 
 import java.util.ArrayList;
@@ -9,10 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- *
- * @author Admin
- */
 public class LopHoc {
     String className;
     List<SinhVien> DSSV;
