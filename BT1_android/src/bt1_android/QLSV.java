dffb80fc3d9a4f96b244ec6c9755af6a57f746be
Lmhieu28 <146105252+Lmhieu28@users.noreply.github.com>
2025-02-24 08:49:00 +0700

    LMH Update QLSV.java

--- BT1_android/src/bt1_android/QLSV.java
+++ BT1_android/src/bt1_android/QLSV.java
@@ -176,6 +176,7 @@ public class QLSV extends JFrame {
         class2.addStudent(new SinhVien("Nguyen", "Quoc Bao", "2001-04-02", "HCM", "CNTT2", grades2_1));
         class2.addStudent(new SinhVien("Le", "Duc Loi", "2001-11-09", "HCM", "CNTT2", grades2));
         class2.addStudent(new SinhVien("Le", "Bao Han", "2001-02-21", "HCM", "CNTT2", grades2_2));
+        class2.addStudent(new SinhVien("Duc", "Hoang", "2001-03-21", "HCM", "CNTT2", grades2));
         //DONE THÊM SV CNTT2
 
 
