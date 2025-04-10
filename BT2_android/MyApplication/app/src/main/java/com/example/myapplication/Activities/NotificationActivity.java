9948c6bb354d60107a11cf4b49a7e7a227dbea35
HUYcupapa <2251061796@e.tlu.edu.vn>
2025-04-10 13:56:04 +0700

    Giao diện thông báo

---
+++ BT2_android/MyApplication/app/src/main/java/com/example/myapplication/Activities/NotificationActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.myapplication.Adapter.NotificationAdapter;
+import com.example.myapplication.Model.Notification;
+import com.example.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationActivity extends AppCompatActivity {
+
+}
