c74efb0e58e33ce6bf9bd271abf9026cdb260ca9
vietcuong2004 <vietcuongtanhoa20062004@gmail.com>
2025-04-11 11:02:51 +0700

    sửa giao diện

--- BT2_android/MyApplication/app/src/main/java/com/example/myapplication/Activities/HomeActivity.java
+++ BT2_android/MyApplication/app/src/main/java/com/example/myapplication/Activities/HomeActivity.java
@@ -2,12 +2,14 @@ package com.example.myapplication.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -63,6 +65,8 @@ public class HomeActivity extends AppCompatActivity {
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        // Đổi màu icon menu sang trắng
+        toggle.getDrawerArrowDrawable().setColor(ContextCompat.getColor(this, android.R.color.white));
 
         // Xử lý sự kiện Navigation Drawer
         navigationView.setNavigationItemSelectedListener(item -> {
